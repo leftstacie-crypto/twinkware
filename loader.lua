d06289e694a43c3c07dc734ce8b72a9ff4fdd3ba
leftstacie-crypto <leftstacie@gmail.com>
2025-12-01 21:39:25 +0000

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,16 @@
+writefile("Twinkware.txt", script_key)
+
+do
+    if game.gameId == 3747388906 then -- // fallen survival 
+        pcall(function()
+            if not game:IsLoaded() then
+                repeat
+                    game.Loaded:Wait()
+                until game:IsLoaded()
+            end
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/leftstacie-crypto/twinkware/refs/heads/main/memorybypass.lua"))()
+            script_key = readfile("Twinkware.txt")
+            loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e4cefa42217d7c4274c9efd11ddc0c1d.lua"))()
+        end)
+	end
+end

03c8478491a734b9cdf2f6567e22253783dfe6c8
leftstacie-crypto <leftstacie@gmail.com>
2025-12-10 16:43:51 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -13,4 +13,10 @@ do
             loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e4cefa42217d7c4274c9efd11ddc0c1d.lua"))()
         end)
 	end
+
+	if game.gameId == 3747388906 then -- // Project Delta
+	end
+
+	if game.gameId == 3747388906 then -- // Apoc Rising 2
+	end
 end
